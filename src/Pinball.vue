f92abcbd8aeec329aa1e38b7f51182abd9bb7985
svenskaskolan <swedishlandlord@hotmail.com>
2020-11-26 00:26:40 +0100

    separate kalmar images from others

--- src/Pinball.vue
+++ src/Pinball.vue
@@ -1,10 +1,9 @@
 <template>
     <head>
         <meta charset="utf-8">
-
         <title>Pinball Tutorials</title>
     </head>
-  <body>
+    <body>
         <div id="app" class="u-mainNormal">
             <div class="heading">
                 <div class="u-inlineBlock u-marginR u-alignMiddle u-maxWidth40">
@@ -79,38 +78,11 @@ export default class Pinball extends Vue {
     viewStyle: string = "list";
     pinData: PinTable[] = [
         {
-        "tag": "6M$M",
-        "name": "The Six Million Dollar Man",
-        "links": [{
-            "linkhref": "https://youtu.be/W1VMFpTqHSY?t=36"
-            }]
-        },
-        {
         "tag": "8B",
         "name": "Eight Ball",
         "image": "eight ball-bally-760.jpg",
         "links": [{
             "linkhref": "https://youtu.be/-wGicmY0lYk?t=5"
-            }]
-        },
-        {
-        "tag": "ACDC",
-        "name": "AC/DC",
-        "links": [{
-            "linkhref": "https://youtu.be/kxkgnSTltVE?t=261"
-            },{
-            "linkhref": "https://youtu.be/XUnw9ogKkYA?t=24029"
-            },{
-            "linkhref": "https://youtu.be/q90L2NTiObo?t=125"
-            },{
-            "linkhref": "videos\\Part5BPO-X.mp4#t=06:42:00"
-            }]
-        },
-        {
-        "tag": "AERO",
-        "name": "Aerosmith",
-        "links": [{
-            "linkhref": "https://youtu.be/oej9gql5OTU?t=559"
             }]
         },
         {
@@ -140,13 +112,6 @@ export default class Pinball extends Vue {
             }]
         },
         {
-        "tag": "BKSOR",
-        "name": "Black Knight: Sword of Rage",
-        "links": [{
-            "linkhref": "https://youtu.be/lyPG3357htE?t=396"
-            }]
-        },
-        {
         "tag": "BSD",
         "name": "Bram Stoker's Dracula",
         "image": "bram stokers dracula-williams-3072.jpg",
@@ -155,35 +120,11 @@ export default class Pinball extends Vue {
             }]
         },
         {
-        "tag": "BTDK",
-        "name": "Batman the dark Knight",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/HOXy2xpY25o?t=43"
-            }]
-        },
-        {
-        "tag": "BURO",
-        "name": "Buck Rogers",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/lePevjDCOVE?t=12"
-            }]
-        },
-        {
         "tag": "CD",
         "name": "Count-Down",
         "image": "count down-gottlieb-573.jpg",
         "links": [{
             "linkhref": "https://youtu.be/OivgT9NXxbI?t=326"
-            }]
-        },
-        {
-        "tag": "CFTBL",
-        "name": "Creature from the Black Lagoon",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/USGjjXcjM5w?t=668"
             }]
         },
         {
@@ -203,37 +144,11 @@ export default class Pinball extends Vue {
             }]
         },
         {
-        "tag": "CV",
-        "name": "Cirqus Voltaire",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/JP5Axk3VLic?t=594"
-            }]
-        },
-        {
         "tag": "DH",
         "name": "Dirty Harry",
         "image": "dirty harry-williams-684.jpg",
         "links": [{
             "linkhref": "https://youtu.be/fm0WXpLxMnM?t=64"
-            }]
-        },
-        {
-        "tag": "DM",
-        "name": "Demolition Man",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/ylSY_jQUjs0?t=5069"
-            },{
-            "linkhref": "videos\\Part6BPO-X.mp4#t=07:56:35"
-            }]
-        },
-        {
-        "tag": "DP",
-        "name": "Deadpool",
-        "image": "",
-        "links": [{
-            "linkhref": "videos\\2019SternProCircuit1.mp4#t=01:06:30"
             }]
         },
         {
@@ -261,40 +176,6 @@ export default class Pinball extends Vue {
             }]
         },
         {
-        "tag": "FH",
-        "name": "Funhouse",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/yY0GJPinballXco?t=327"
-            }]
-        },
-        {
-        "tag": "FLG",
-        "name": "Flash Gordon",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/ylSY_jQUjs0?t=25619"
-            }]
-        },
-        {
-        "tag": "FRONT",
-        "name": "Frontier",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/7nGOwi7qJgM?t=4690"
-            }]
-        },
-        {
-        "tag": "FS",
-        "name": "The Flintstones",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/XUnw9ogKkYA?t=28712"
-            },{
-            "linkhref": "videos\\Part5BPO-X.mp4#t=08:02:40"
-            }]
-        },
-        {
         "tag": "FT",
         "name": "Fish Tales",
         "image": "fish tales-williams-861.jpg",
@@ -304,44 +185,6 @@ export default class Pinball extends Vue {
             "linkhref": "videos\\Part5BPO-X.mp4#t=08:02:40"
             },{
             "linkhref": "videos\\Part5BPO-X.mp4#t=08:25:25"
-            }]
-        },
-        {
-        "tag": "GB",
-        "name": "Ghostbusters",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/XUnw9ogKkYA?t=267"
-            }]
-        },
-        {
-        "tag": "GEN?",
-        "name": "Genesis",
-        "image": "",
-        "links": [{
-            "linkhref": "videos\\PAPA20Finals2017.mp4#t=00:03:40"
-            },{
-            "linkhref": "https://youtu.be/Y8c43MU7rqs?t=218"
-            }]
-        },
-        {
-        "tag": "GOT",
-        "name": "Game of Thrones",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/XUnw9ogKkYA?t=22478"
-            },{
-            "linkhref": "videos\\Part5BPO-X.mp4#t=06:17:30"
-            },{
-            "linkhref": "videos\\Part5BPO-X.mp4#t=08:08:50"
-            }]
-        },
-        {
-        "tag": "GOTG",
-        "name": "Guardians of the Galaxy",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/fk6rAZ0vARo?t=10882"
             }]
         },
         {
@@ -355,7 +198,7 @@ export default class Pinball extends Vue {
         {
         "tag": "HS2",
         "name": "Getaway: High Speed II",
-        "image": "",
+        "image": "the getaway high speed2-williams-1000.jpg",
         "links": [{
             "linkhref": "https://youtu.be/y_BgVLzT3I0?t=632"
             }]
@@ -366,16 +209,6 @@ export default class Pinball extends Vue {
         "image": "indianapolis 500-bally-2853.jpg",
         "links": [{
             "linkhref": "https://youtu.be/0JJ58ZBaBxo?t=96"
-            }]
-        },
-        {
-        "tag": "IM",
-        "name": "Iron Man",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/XUnw9ogKkYA?t=10551"
-            },{
-            "linkhref": "videos\\Part6BPO-X.mp4#t=05:57:20"
             }]
         },
         {
@@ -391,6 +224,12 @@ export default class Pinball extends Vue {
             },{
             "linkhref": "videos\\IFPA16.mp4#t=02:09:30"
             }]
+        },        
+        {
+        "tag": "JM",
+        "name": "Johnny Mnemonic",
+        "image": "johnny mnemonic-williams-3683.jpg",
+        "links": []
         },
         {
         "tag": "JOP",
@@ -406,50 +245,6 @@ export default class Pinball extends Vue {
         "image": "jurassic park-stern-6573.jpg",
         "links": [{
             "linkhref": "https://youtu.be/wsHKv8PFk3s?t=146"
-            }]
-        },
-        {
-        "tag": "JQ",
-        "name": "Jungle Queen",
-        "image": "",
-        "links": [{
-            "linkhref": "videos\\PAPA20Finals2017.mp4#t=06:36:40"
-            },{
-            "linkhref": "videos\\PAPA20Finals2017.mp4#t=02:09:30"
-            }]
-        },
-        {
-        "tag": "JY",
-        "name": "Junk Yard",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/pe1C9LUxn6k?t=281"
-            }]
-        },
-        {
-        "tag": "KISS",
-        "name": "Kiss (Bally)",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/XUnw9ogKkYA?t=24849"
-            },{
-            "linkhref": "videos\\Part5BPO-X.mp4#t=06:55:10"
-            }]
-        },
-        {
-        "tag": "LOTR",
-        "name": "The Lord of the Rings",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/zttZPQy1vnk?t=501"
-            }]
-        },
-        {
-        "tag": "LW",
-        "name": "Lost World",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/xMHuvYiDMxI?t=9"
             }]
         },
         {
@@ -493,22 +288,6 @@ export default class Pinball extends Vue {
             }]
         },
         {
-        "tag": "NGG",
-        "name": "No Good Gofers",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/kNOWXKKNqRw?t=71"
-            }]
-        },
-        {
-        "tag": "PAR",
-        "name": "Paragon",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/S75vg7Wtf7w?t=57"
-            }]
-        },
-        {
         "tag": "POWP",
         "name": "Power Play",
         "image": "bobby orr power play-bally-1858.jpg",
@@ -517,37 +296,25 @@ export default class Pinball extends Vue {
             }]
         },
         {
-        "tag": "RAD!",
-        "name": "Radical!",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/iqyxn1EH1OA?t=51"
-            }]
+        "tag": "PP",
+        "name": "Pinball Pool",
+        "image": "pinball pool-gottlieb-1795.jpg",
+        "links": []
+        },
+        {
+        "tag": "PZ",
+        "name": "Party Zone",
+        "image": "party zone-bally-1764.jpg",
+        "links": []
         },
         {
         "tag": "RS",
         "name": "Road Show",
-        "image": "",
+        "image": "red and teds roadshow-williams-1972.jpg",
         "links": [{
             "linkhref": "videos\\Part5BPO-X.mp4#t=05:15:20"
             },{
             "linkhref": "https://youtu.be/XUnw9ogKkYA?t=1825"
-            }]
-        },
-        {
-        "tag": "SBM",
-        "name": "Silverball Mania",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/EGNRddj_ZU0?t=430"
-            }]
-        },
-        {
-        "tag": "SHREK",
-        "name": "Shrek",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/AJRwpatZR-w?t=1"
             }]
         },
         {
@@ -567,34 +334,16 @@ export default class Pinball extends Vue {
         {
         "tag": "SM",
         "name": "Spider-man",
-        "image": "",
+        "image": "the amazing spiderman-gottlieb-2285.jpg",
         "links": [{
             "linkhref": "https://youtu.be/5mqTABBa3fI?t=5012"
             }]
         },
         {
-        "tag": "SNS",
-        "name": "Strikes N Spares",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/6Q9mWnumgc8?t=19"
-            }]
-        },
-        {
-        "tag": "SOLR",
-        "name": "Solar Ride",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/x_CYy_mICZ4?t=226"
-            }]
-        },
-        {
-        "tag": "SS",
-        "name": "Scared Stiff",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/YOz4RxI-vnQ?t=334"
-            }]
+        "tag": "SB",
+        "name": "Sinbad",
+        "image": "sinbad-gottlieb-2159.jpg",
+        "links": []
         },
         {
         "tag": "ST-B",
@@ -605,53 +354,11 @@ export default class Pinball extends Vue {
             }]
         },
         {
-        "tag": "SW",
-        "name": "Star Wars",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/fk6rAZ0vARo?t=42274"
-            }]
-        },
-        {
-        "tag": "T2",
-        "name": "Terminator 2",
-        "image": "",
-        "links": [{
-            "linkhref": "videos\\Part6BPO-X.mp4#t=06:47:52"
-            }]
-        },
-        {
-        "tag": "TF",
-        "name": "Transformers",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/bFKvbsyt6XI?t=63"
-            }]
-        },
-        {
-        "tag": "TNA",
-        "name": "Total Nuclear Annihilation",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/s6ZaHk1Fbys?t=57"
-            }]
-        },
-        {
         "tag": "TOM",
         "name": "Theatre of Magic",
-        "image": "",
+        "image": "the theatre of magic-bally-2845.jpg",
         "links": [{
             "linkhref": "https://youtu.be/BzLXJMNdoXs?t=69"
-            }]
-        },
-        {
-        "tag": "TOTAN",
-        "name": "Tales of the Arabian Nights",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/XUnw9ogKkYA?t=252"
-            },{
-            "linkhref": "videos\\Part6BPO-X.mp4#t=07:30:5"
             }]
         },
         {
@@ -660,14 +367,6 @@ export default class Pinball extends Vue {
         "image": "disney tron legacy-stern-5682.jpg",
         "links": [{
             "linkhref": "https://youtu.be/8u1bVbwYy0U?t=21"
-            }]
-        },
-        {
-        "tag": "TS",
-        "name": "The Shadow",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/fMxSmlqRqtE?t=816"
             }]
         },
         {
@@ -681,55 +380,11 @@ export default class Pinball extends Vue {
             }]
         },
         {
-        "tag": "TZ",
-        "name": "Twilight Zone",
-        "image": "",
-        "links": [{
-            "linkhref": "videos\\PAPA20Finals2017.mp4#t=01:41:00"
-            },{
-            "linkhref": "https://youtu.be/Y8c43MU7rqs?t=2135"
-            }]
-        },
-        {
         "tag": "WCS",
         "name": "World Cup Soccer",
-        "image": "",
+        "image": "world cup soccer-williams-2811.jpg",
         "links": [{
             "linkhref": "https://youtu.be/ylSY_jQUjs0?t=14725"
-            }]
-        },
-        {
-        "tag": "VIK",
-        "name": "Viking",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/hDu9g6RrI8g?t=840"
-            }]
-        },
-        {
-        "tag": "WM",
-        "name": "Wrestlemania",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/hFXAC7A2Etg?t=284"
-            }]
-        },
-        {
-        "tag": "WPT",
-        "name": "World Poker Tour",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/Y8c43MU7rqs?t=3987"
-            },{
-            "linkhref": "videos\\PAPA20Finals2017.mp4#t=01:06:30"
-            }]
-        },
-        {
-        "tag": "WW",
-        "name": "White Water",
-        "image": "",
-        "links": [{
-            "linkhref": "https://youtu.be/Z7YOug3f9a4?t=2"
             }]
         },
         {
